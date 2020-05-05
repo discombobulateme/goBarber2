005d61263231f34cc3fd0bafe9a3228f645eaaaf
discombobulateme <pcultural@gmail.com>
2020-05-05 12:13:06 +0200

    add authentication session route, token

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,46 @@
+// This is the authentication route
+
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  // const { email, password } = request.body;
+
+  // const authenticateUser = new AuthenticateUserService();
+
+  // // /*
+  // // we never get the response of a service and send it to return,
+  // // specifically when it's a generic name, that I cannot understand what its returning
+  // // I need to know what am I receiving from service and sending to frontend
+  // // that's why my const is {user} and not something like: response
+  // // */
+  // // const { user, token } = await authenticateUser.execute({
+  //   const { user } = await authenticateUser.execute({
+  //   email,
+  //   password,
+  // });
+
+  // delete user.password;
+
+  // return response.json({ user, token });
+
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+      const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+    delete user.password;
+    return response.json({ user, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,58 @@
+import { getRepository } from 'typeorm'; // as this repo has no specific rule, we can use this default
+import { compare } from 'bcryptjs'; // import hash function from crypto library
+import { sign } from 'jsonwebtoken';
+// import authConfig from '../config/auth';
+
+// import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    // I'm searching for a user in the repository that uses email as a search parameter
+    const user = await usersRepository.findOne({ where: { email } });
+
+    // because findOne method returns user or undefined
+    // we don't need to compare, a simple: not(!) user is enough
+    if (!user) {
+      // for security purpose error doesnt'inform what actually is incorrect
+      // throw new AppError('Incorrect email/ password combination', 401);
+      throw new Error('Incorrect email/ password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      // throw new AppError('Incorrect email/ password combination', 401);
+      throw new Error('Incorrect email/ password combination');
+    }
+
+    // // if pass through all those validations, I have an authenticated user
+    // const { secret, expiresIn } = authConfig.jwt;
+
+    // secret key generated with md5 online
+    const token = sign({}, '5247c254c0efef32f448be3045aa781f', {
+      subject: user.id, // connects key to user
+      expiresIn: '1d', // allow user to unlog! good for security
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
