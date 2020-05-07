099679ac9a3c721866d1767a187e8803ad690629
discombobulateme <pcultural@gmail.com>
2020-05-07 11:27:55 +0200

    add global errors handling

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -34,24 +34,16 @@ usersRouter.patch(
   ensureAuthenticated, // first middleware
   upload.single('avatar'), // second middleware
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
-      delete user.password;
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
-
-    // delete user.password;
-
-    // return response.json(user);
-  },
+    return response.json(user);
+  }
 );
 
 export default usersRouter;
