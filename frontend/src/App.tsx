d86dcb4400c84856be7f2d4265adc1520e03fe57
discombobulateme <pcultural@gmail.com>
2020-05-13 11:16:10 +0200

    cors add to backend + fronted inputs

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import GlobalStyle from './styles/global';
+
+// import { AuthProvider } from './hooks/AuthContext';
+
+const App: React.FC = () => (
+  <h1>GoBarber App</h1>
+  // <>
+  //   <AuthProvider>
+  //     <SignIn />
+  //   </AuthProvider>
+
+  //   <GlobalStyle />
+  // </>
+);
+
+export default App;
